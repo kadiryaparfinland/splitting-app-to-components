cafa651c6ce6b90d9576dbe4f1de878ddf53f8ba
andromegasoftware <kadiryapar16@gmail.com>
2023-03-14 12:18:01 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'cafe';
+export class AppComponent implements OnInit{
+  tarjoiluOhje: any = [];
+  tyo = '';
+  poydanNumero = '';
+  myyntiMaara = '';
+  faCoffee = faCoffee;
+
+
+  ngOnInit() {
+    
+  }
+  kahviaMyyty() {
+    this.tarjoiluOhje.push({
+      tyo: 'myyty',
+      poydanNumero: this.poydanNumero,
+      myyntiMaara: this.myyntiMaara
+    });
+  }
+
+  kahviaTarjoiltu() {
+    this.tarjoiluOhje.push({
+      tyo: 'tarjoiltu',
+      poydanNumero: this.poydanNumero,
+      myyntiMaara: this.myyntiMaara
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,
+  ]
 })
 export class AppModule { }
